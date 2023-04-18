753fc937b4e704a5defe6b85b8f6b9d0b2bf99f6
Ankan Basu <an.basu.kan@gmail.com>
2023-04-19 02:07:24 +0530

    route name change

--- routes/dataRoutes.js
+++ routes/dataRoutes.js
@@ -5,6 +5,6 @@ const router = express.Router();
 router.get('/top-sights/', dataControllers.getTopSights);
 router.get('/hotels/', dataControllers.getHotels);
 router.get('/desc/', dataControllers.getDesc);
-router.get('/search/', dataControllers.getSearch);
+router.get('/search2/', dataControllers.getSearch);
 
 module.exports = router;
